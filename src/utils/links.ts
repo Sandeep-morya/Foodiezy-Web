4396c2d66d1071f85d4710dce25bec9c7c8ebe00
Sandeep Morya <saabmaurya@gmail.com>
2024-04-12 14:13:15 +0000

    fixed image issue

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,13 +1,13 @@
 ﻿export const ORIGINAL_IMG_LINK =
-	"https://res.cloudinary.com/swiggy/image/upload/";
+	"https://media-assets.swiggy.com/swiggy/image/upload/";
 export const HD_IMG_LINK =
-	"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/";
+	"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/";
 export const SD_IMG_LINK =
-	"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_640/";
+	"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_640/";
 export const MD_IMG_LINK =
-	"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_420/";
+	"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_420/";
 export const LOW_IMG_LINK =
-	"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_180/";
+	"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_180/";
 
 export const RESTAURANT_LINK =
 	"https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true";
